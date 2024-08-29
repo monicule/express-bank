a00d1e2bbdcdf5f6dd1a59081aa29aa13af29c9f
Monika <laiskutis.monikai@gmail.com>
2024-08-29 15:02:51 +0300

    accounts

--- data/accounts.js
+++ data/accounts.js
@@ -1,12 +1,12 @@
 export const accounts = [
     {
-        firstName: 'Jonas',
-        lastName: 'Jonaitis',
-        dayOfBirth: '2000-01-01',
+        name: 'Jonas',
+        surname: 'Jonaitis',
+        dob: '2000-01-01',
     },
     {
-        firstName: 'Petras',
-        lastName: 'Petraitis',
-        dayOfBirth: '2002-02-02',
+        name: 'Petras',
+        surname: 'Petraitis',
+        dob: '2002-02-02',
     },
 ];
