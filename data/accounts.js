cf96263f74a9831e13263c52cc3978215ecc8c27
Monika <laiskutis.monikai@gmail.com>
2024-08-29 09:55:14 +0300

    accounts

--- data/accounts.js
+++ data/accounts.js
@@ -1,1 +1,12 @@
-export const accounts = [];
+export const accounts = [
+    {
+        firstName: 'Jonas',
+        lastName: 'Jonaitis',
+        dayOfBirth: '2000-01-01',
+    },
+    {
+        firstName: 'Petras',
+        lastName: 'Petraitis',
+        dayOfBirth: '2002-02-02',
+    },
+];
--- index.js
+++ index.js
@@ -8,16 +8,27 @@ app.use(express.json({
     type: 'application/json',
 }));
 
-app.use('/api', apiRouter);
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     return res.send('Bank');
-})
+});
+
+app.use('/api', apiRouter);
 
 app.get('*', (req, res) => {
     return res.send('404 - puslapis nerastas...');
-})
+});
+
+app.use((req, res, next) => {
+    return res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
-    console.log(`App running on http://localhost:${port}`);
-})
+    console.log(`Serveris pasileido: http://localhost:${port}`);
+});
