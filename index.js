b6e60fb2a2f31b9a309c66e0e50bbf2ac519896e
Monika <laiskutis.monikai@gmail.com>
2024-09-02 09:01:57 +0300

    update

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ import { apiRouter } from "./router/apiRouter.js";
 const app = express();
 const port = 5030;
 
-app.use(express.json({
-    type: 'application/json',
-}));
+app.use(express.json({ type: 'application/json', }));
 
 app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', apiRouter);
 
 app.get('/', (req, res) => {
     return res.send('Bankas. Sveiki atvykę!')
 });
+
+app.use('/api', apiRouter);
 
 app.get('*', (req, res) => {
     return res.send('404 - puslapis nerastas.');
